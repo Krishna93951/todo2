1d4348d023782208e0e9fb3c8f9ded35bbbba48c
Krishna93951 <kkagile123@gmail.com>
2019-09-24 14:18:46 +0530

    3 files modified

--- index.js
+++ index.js
@@ -4,118 +4,143 @@ import './style.css';
 // Write Javascript code!
 var todos = [];
 var todos1 = [];
+//Count Messages if tasks are available
 var localeString = {
   all: "Number of All Tasks:",
   completed: "Number of Completed Tasks:",
   pending: "Number of Pending Tasks: "
 };
-var empty={
-  all:"No Tasks Available",
-  completed:"No"
-}
-function init(){
+////Messages if tasks are unavailable
+var emptyString= {
+  empty:"No Tasks Available",
+  noneCompleted:"None of the Tasks are Completed",
+  nonePending:"None of the Tasks are Pending"
+};
+
+function init()
+{
   showTodo();
   eventHandler();
 }
-function getTodo() {
+function getTodo() 
+{
   var todos_str = localStorage.getItem('todo');
-  if (todos_str != null) {
+  if (todos_str != null) 
+  {
     todos = JSON.parse(todos_str);
   }
   return todos;
 }
-function addTodo() {
+function addTodo() 
+{
   var task = document.getElementById('todoTask').value;
   var todos = getTodo();
   todos.push(task);
   localStorage.setItem('todo', JSON.stringify(todos));
   console.log(task);
   showTodo();
-
-
 }
-function showTodo() {
+function showTodo() 
+{
   var todos = getTodo();
-  var html = '<ul>';
-  for (var i = 0; i < todos.length; i++) {
-    html += '<li>' + '<input type="checkbox" id="' + i + '">' + todos[i] + '<button class="remove" id="' + i + '">Del</button></li>';
+  var html = '<ul id="ul">';
+  for (var i = 0; i < todos.length; i++)
+  {
+    html += '<li>' + '<input type="checkbox" id="' + i + '" class="check">' +' '+todos[i] +'  '+'<button class="remove" id="' + i + '">Del</button></li>';
   };
   html += '</ul>';
   document.getElementById('todoList').innerHTML = html;
   removeBtn();
 }
-function removeTodo() {
+function removeTodo() 
+{
   var id = this.getAttribute('id');
   var todos = getTodo();
   todos.splice(id, 1);
   localStorage.setItem('todo', JSON.stringify(todos));
   showTodo();
 }
-function all() {
+//function for all tasks
+function all() 
+{
   var todos = getTodo();
   todos1 = todos;
-  if (todos1.length != 0) {
+  if (todos1.length != 0) 
+  {
     alert(localeString.all + todos1.unshift());
   }
-  else {
-    alert("No Tasks available");
+  else 
+  {
+    alert(emptyString.empty);
   }
-  console.log(todos1);
 }
-function checked() {
-  var completed = document.querySelectorAll('input[type="checkbox"]:checked')
+//function for completed tasks
+function checked() 
+{
+  var completed = document.querySelectorAll('input[type="checkbox" ]:checked')
   //console.log(Object.keys(comp));
   //console.log(Object.entries(comp));
-
-  if (completed.length != 0) {
-    alert(localeString.completed + ' ' + completed.length)
+  if (completed.length != 0) 
+  {
+    alert(localeString.completed + ' ' + completed.length);
   }
-  else {
-    alert("None are completed")
+  else
+  {
+    alert(emptyString.noneCompleted);
   }
 }
-function pending() {
+//function for pending tasks
+function pending() 
+{
   var completed = document.querySelectorAll('input[type="checkbox"]:checked')
   var todos = getTodo();
-  todos1 = todos;
-  var pending = todos1.length - completed.length;
-  alert(localeString.pending + ' ' + pending);
+  var pending = todos.length - completed.length;
+  //console.log(pending)
+  if(pending != 0)
+  {
+    alert(localeString.pending + ' ' + pending);
+  }
+  else
+  {
+    alert(emptyString.nonePending);
+  }
 }
-function clearField() {
+//clearing the text field
+function clearField() 
+{
   document.getElementById("todoTask").value = "";
 }
-function focusField() {
+//focusing on the text field
+function focusField() 
+{
   document.getElementById("todoTask").focus();
 }
-function removeBtn() {
+function removeBtn() 
+{
   var buttons = document.getElementsByClassName('remove');
   for (var i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', removeTodo);
   };
 }
-function eventHandler() {
-  document.getElementById('addBtn').addEventListener('click', event => {
+function eventHandler() 
+{
+  document.getElementById('addBtn').addEventListener('click', event => 
+  {
     focusField();
     addTodo();
     clearField();
   });
-  document.getElementById('all').addEventListener('click', event => {
-
+  document.getElementById('all').addEventListener('click', event => 
+  {
     all();
-
-
   });
-  document.getElementById('completed').addEventListener('click', event => {
-
+  document.getElementById('completed').addEventListener('click', event => 
+  {
     checked();
-
-
   });
-  document.getElementById('pending').addEventListener('click', event => {
-
+  document.getElementById('pending').addEventListener('click', event => 
+  {
     pending();
-
-
   });
 }
 init();
