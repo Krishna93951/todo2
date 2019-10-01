59d7e01e7896c986cb1317ad5575a9fa08070426
Krishna93951 <kkagile123@gmail.com>
2019-10-01 20:07:07 +0530

    1 file modified

--- model.js
+++ model.js
@@ -8,7 +8,20 @@ function getTodo()
   return todos;
 }
 
-
 function setTodo(){
   sessionStorage.setItem('todo', JSON.stringify(todos));
+}
+
+function getTodoFromLocalStorage() 
+{
+  var todos_str = localStorage.getItem('todo');
+  if (todos_str != null) 
+  {
+    todos = JSON.parse(todos_str);
+  }
+  return todos;
+}
+
+function setTodoInLocalStorge(){
+  localStorage.setItem('todo', JSON.stringify(todos));
 }
